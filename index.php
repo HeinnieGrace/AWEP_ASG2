7192b24facc7574d67d63d386c9a2be1cb528355
HeinnieGrace <20ftt1442@student.pb.edu.bn>
2022-02-23 09:52:03 +0800

    for homepage

--- index.php
+++ index.php
@@ -4,9 +4,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>index</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    
+    <head>
+        <div id="menu">
+            <div id="logo">
+                <p>POLITEKNIK BRUNEI</p>
+            </div>
+            <div id="allButtons">
+                <div class="menuButton">
+                    <a href="#"><button>Home</button></a>
+                </div>
+                <div class="menuButton">
+                    <a href="#"><button>Rules</button></a>
+                </div>
+                <div class="menuButton">
+                    <a href="#"><button>About Us</button></a>
+                </div>
+                <div class="menuButton">
+                    <a href="#"><button>Contact Us</button></a>
+                </div>
+            </div>
+        </div>
+    <head>
+    <main>
+        <div id="homepage">
+            <h3 class="upText llm">Library Locker Management</h3>
+            <div id="topHome">
+                <div id="slogan">
+                    <div id="bookTitle">
+                        <p class="first">BOOK YOUR</p><p class="second">LOCKER</p><p class="first">NOW!</p>
+                    </div>
+                    <div id="littleSlogan">
+                        <p>Why bring a lot of stuff everyday when you can keep it at school.</p>
+                    </div>
+                    <button>Book a Locker</button>
+                </div>
+                <div id="locker">
+                    <div id="outside">
+                        <div class="inside">
+                            <div>
+                                
+                            </div>
+                            <img src="assets/unlock.png">
+                        </div>
+                        <div class="inside">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+           <?php
+
+            // for ($i=0; $i<100; $i++) {
+            //     echo "<p>Hello world</p> ";
+            // }
+
+           ?>
+        </div>
+        
+    </main>
 </body>
 </html>
