a622feef295e8d1f687db6c3cfb17b33f63df3ce
DahliaSungai <98628001+DahliaSungai@users.noreply.github.com>
2022-02-26 11:24:29 +0800

    Update index.js
    Function for hidden - contact us . 
    From : Hana

--- js/index.js
+++ js/index.js
@@ -24,61 +24,9 @@ function blockCashJS() {
     console.log("Cash/Receipt");  
 }
 
-
-//clear the contact us fields
-function clearFields() {
-    document.getElementById("fullname").value=""
-    document.getElementById("phone").value=""
-    document.getElementById("phone").value=""
-    document.getElementById("description").value=""
-}
-//validate contact us
-var submitForm = document.getElementById("submitform");
-	
-	submitForm.addEventListener("click", function(){
-
-		checkfield();
-	});
-
-	var fullname = document.getElementById("fullname");
-	var phone = document.getElementById("phone");
-	var fullname= document.getElementById("fullname");
-    var email = document.getElementById("email");
-    var description = document.getElementById("description");
-
-	fullname.addEventListener("keyup", function(){
-		checkInput(this.value,0);
-	});
-	phone.addEventListener("keyup", function(){
-		checkInput(this.value,1);
-	});
-	fullname.addEventListener("keyup", function(){
-		checkInput(this.value,2);
-	});
-    email.addEventListener("keyup", function(){
-		checkInput(this.value,3);
-	});
-    description.addEventListener("keyup", function(){
-		checkInput(this.value,4);
-	});
-
-
-function checkfield(){
-
-		var userinput = document.getElementsByTagfullname("input");
-		var error = document.getElementsByTagfullname("span");
-		var output = document.getElementById("output");
-
-		output.innerHTML="";
-
-		for (var i = 0; i < userinput.length; i++){
-			if(!userinput[i].value){
-				error[i].style.visibility="visible";
-			}
-			else{
-				error[i].style.visibility="hidden";
-			}
-
-			output.innerHTML = output.innerHTML + (userinput[i].value? userinput[i].value + "<br>": "");
-		}
+//when submit on ContactUs
+function submitContact() {
+	alert("Thank you for contacting us. A copy of your submission has been send to your email. We will respond to you soon.");
+        document.getElementById("main").style.display = "none";
+        document.getElementById("output").style.display = "block";
 }
