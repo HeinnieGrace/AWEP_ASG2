ec1b9310f65cb4d9ebe80c46a04f5a8a18964c5e
DahliaSungai <98628001+DahliaSungai@users.noreply.github.com>
2022-02-26 11:30:22 +0800

    Update contactus.php
    alert error ~

--- contactus.php
+++ contactus.php
@@ -32,30 +32,40 @@
             </div>
         </div>
     <head>
-    <div id="main"> 
+        <div id="main"> 
     <form method="POST" action="">
     <h1 style="color:#8888">DO YOU HAVE ANY ENQUIRES?</h1>
     <table class="enquires">
         <tr>
             <td><label for="fullname">fullname:<span class="require">*</span></label></td>
-            <td><input type="text" id="fullname" class="inputForm" name="fullname" placeholder="Enter your name"><span class='msg' id='name-error'></span></td>
+            <td><input type="text" id="fullname" class="inputForm" name="fullname" placeholder="Enter your name"><span class='msg' id='name-error' required></span></td>
         </tr>
         <tr>
             <td><label for="phone">Phone Number:<span class="require">*</span></label></td>
-            <td><input type="tel" id="phone" class="inputForm" name="phone" pattern="(2|3|4|5|7|8){1}[0-9]{6}" maxlength='7' placeholder="Eg: 8888888"><span class='msg' id='phone-error'></span></td>
+            <td><input type="tel" id="phone" class="inputForm" name="phone" pattern="(2|3|4|5|7|8){1}[0-9]{6}" maxlength='7' placeholder="Eg: 8888888"><span class='msg' id='phone-error' required></span></td>
         </tr>
         <tr>
             <td><label for="email">Email:<span class="require">*</span></label></td>
-            <td><input type="email" id="email" class="inputForm" name="email" placeholder="Enter your Email"><span class='msg' id='email-error'></span></td>
+            <td><input type="email" id="email" class="inputForm" name="email" placeholder="Enter your Email"><span class='msg' id='email-error' required></span></td>
         </tr>
         <tr>
             <td><label for='description'>Description</label><span class="require">*</span></td>
-            <td><textarea rows="4" cols="50" id='description' class="inputForm" name='description'></textarea><span class='msg' id='description-error'></span></td>
+            <td><textarea rows="4" cols="50" id='description' class="inputForm" name='description'></textarea><span class='msg' id='description-error' required></span></td>
         </tr>
     </table>
+    <?php
+        if(isset($_POST['submitFormContact'])) {
+            if(isset($_POST['fullname']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['description'])) {
+                $fullname = $_POST['fullname'];
+                $phone = $_POST['phone'];
+                $email = $_POST['email'];
+                $description = $_POST['description'];
+            }
+        }
+    ?>
     <div id="buttonForm">
         <input id="cancelButton" type="button" onclick="clearFields()" value="Clear">
-        <input id="submitform" type="button" onclick="submit()" value="Submit"><span class='msg' id='submit-error'></span>
+        <input id="submitFormContact" type="button" onclick="submitContact()" value="Submit"><span class='msg' id='submit-error'></span>
     </div>
     <div id="output"></div>
     </form>
