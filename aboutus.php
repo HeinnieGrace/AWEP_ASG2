9973b6c95b28797fc40d0acacf420fb9bb74b0b7
HanaBujang <99270513+HanaBujang@users.noreply.github.com>
2022-02-26 12:27:09 +0800

    Update aboutus.php
    updated

--- aboutus.php
+++ aboutus.php
@@ -30,28 +30,28 @@
             </div>
         </div>
     <head>
-    <div id="aboutUs>
-        <h1 style="color:#8B8B8B;">LIBRARY LOCKER MANAGEMENT</h1>
+    <div id="aboutUs">
+        <h1>LIBRARY LOCKER MANAGEMENT</h1>
         <br>
         
-        <b>About Us</b>
-        <p>This locker management system is created in 2022 by 3 amazing Web Development
+        <b style="font-size: 2vw;">About Us</b>
+        <p style="font-size: 1.5vw;">This locker management system is created in 2022 by 3 amazing Web Development
             Course students from intake 10. The aim of this website is to allow students to easily book a locker 
             with less paper work and at the same time providing ease to the library management system.
         </p>
         <br>
         
-        <b>Location of Locker</b>
-        <p>All lockers are stationed at all level of the Politeknik Ong Sum Ping. The reason 
+        <b style="font-size: 2vw;">Location of Locker</b>
+        <p style="font-size: 1.5vw;">All lockers are stationed at all level of the Politeknik Ong Sum Ping. The reason 
             to this choice of location is to make students easily to walk where it is close to them.
         </p>
         <br>
 
-        <b>Web Developers</b>
-        <p><em>DWDT06 AWEP21S2 (Group 2)</em></p>
-        <p>[20FTT1442] XAN HEINNIE GRACE BINTI BADANG</p>
-        <p>[20FTT1448] DAHLIA BINTI SUNGAI</p>
-        <p>[20FTT1452] NUR HANA BINTI BUJANG</p>
+        <b style="font-size: 1.5vw;">Web Developers</b>
+        <p style="font-size: 1.5vw;"><em>DWDT06 AWEP21S2 (Group 2)</em></p>
+        <p style="font-size: 1.5vw;">[20FTT1442] XAN HEINNIE GRACE BINTI BADANG</p>
+        <p style="font-size: 1.5vw;">[20FTT1448] DAHLIA BINTI SUNGAI</p>
+        <p style="font-size: 1.5vw;">[20FTT1452] NUR HANA BINTI BUJANG</p>
     </div>
     <footer>
         <div id="footer">
